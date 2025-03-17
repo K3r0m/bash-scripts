1e9e2e6c78d642a2becd0e86454a624f0d11520e
K3r0m <109816170+K3r0m@users.noreply.github.com>
2025-03-17 19:16:29 +0100

    Update Wan_Install.bash

--- Wan_Install.bash
+++ Wan_Install.bash
@@ -7,11 +7,11 @@ mkdir -p SwarmUI/Models/vae
 mkdir -p SwarmUI/Models/clip_vision
 
 # Dateien nacheinander herunterladen
-curl -L -o SwarmUI/Models/diffusion_models/wan2.1_i2v_720p_14B_fp8_scaled.safetensors https://huggingface.co/Comfy-Org/Wan_2.1_ComfyUI_repackaged/resolve/main/split_files/diffusion_models/wan2.1_i2v_720p_14B_fp8_scaled.safetensors
+curl -L -o SwarmUI/Models/diffusion_models/wan2.1_i2v_480p_14B_fp8_scaled.safetensors https://huggingface.co/Comfy-Org/Wan_2.1_ComfyUI_repackaged/resolve/main/split_files/diffusion_models/wan2.1_i2v_480p_14B_fp8_scaled.safetensors
 
 curl -L -o SwarmUI/Models/clip/umt5_xxl_fp8_e4m3fn_scaled.safetensors "https://huggingface.co/Comfy-Org/Wan_2.1_ComfyUI_repackaged/resolve/main/split_files/text_encoders/umt5_xxl_fp8_e4m3fn_scaled.safetensors"
 
-curl -L -o SwarmUI/Models/vae/wan_2.1_vae.safetensors "https://huggingface.co/Comfy-Org/Wan_2.1_ComfyUI_repackaged/resolve/main/split_files/vae/wan_2.1_vae.safetensors"
+curl -L -o SwarmUI/Models/VAE/wan_2.1_vae.safetensors "https://huggingface.co/Comfy-Org/Wan_2.1_ComfyUI_repackaged/resolve/main/split_files/vae/wan_2.1_vae.safetensors"
 
 curl -L -o SwarmUI/Models/clip_vision/clip_vision_h.safetensors "https://huggingface.co/Comfy-Org/Wan_2.1_ComfyUI_repackaged/resolve/main/split_files/clip_vision/clip_vision_h.safetensors"
 
