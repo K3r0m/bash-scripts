50588c6776d5f07f8d9b0557c73e202144cee070
K3r0m <109816170+K3r0m@users.noreply.github.com>
2025-03-17 19:39:06 +0100

    Update download_models.sh

--- download_models.sh
+++ download_models.sh
@@ -3,15 +3,15 @@
 # Erstelle die Verzeichnisse, falls sie nicht existieren
 mkdir -p /workspace/SwarmUI/Models/diffusion_models
 mkdir -p /workspace/SwarmUI/Models/clip
-mkdir -p /workspace/SwarmUI/Models/vae
+mkdir -p /workspace/SwarmUI/Models/VAE
 mkdir -p /workspace/SwarmUI/Models/clip_vision
 
 # Dateien nacheinander herunterladen
-curl -L -o /workspace/SwarmUI/Models/diffusion_models/wan2.1_i2v_720p_14B_fp8_scaled.safetensors "https://huggingface.co/Comfy-Org/Wan_2.1_ComfyUI_repackaged/blob/main/split_files/diffusion_models/wan2.1_i2v_720p_14B_fp8_scaled.safetensors"
+curl -L -o SwarmUI/Models/diffusion_models/wan2.1_i2v_480p_14B_fp8_scaled.safetensors https://huggingface.co/Comfy-Org/Wan_2.1_ComfyUI_repackaged/resolve/main/split_files/diffusion_models/wan2.1_i2v_480p_14B_fp8_scaled.safetensors
 
 curl -L -o /workspace/SwarmUI/Models/clip/umt5_xxl_fp8_e4m3fn_scaled.safetensors "https://huggingface.co/Comfy-Org/Wan_2.1_ComfyUI_repackaged/resolve/main/split_files/text_encoders/umt5_xxl_fp8_e4m3fn_scaled.safetensors"
 
-curl -L -o /workspace/SwarmUI/Models/vae/wan_2.1_vae.safetensors "https://huggingface.co/Comfy-Org/Wan_2.1_ComfyUI_repackaged/resolve/main/split_files/vae/wan_2.1_vae.safetensors"
+curl -L -o /workspace/SwarmUI/Models/VAE/wan_2.1_vae.safetensors "https://huggingface.co/Comfy-Org/Wan_2.1_ComfyUI_repackaged/resolve/main/split_files/vae/wan_2.1_vae.safetensors"
 
 curl -L -o /workspace/SwarmUI/Models/clip_vision/clip_vision_h.safetensors "https://huggingface.co/Comfy-Org/Wan_2.1_ComfyUI_repackaged/resolve/main/split_files/clip_vision/clip_vision_h.safetensors"
 
