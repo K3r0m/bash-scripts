ca7199da78cbb3146e2666ad820f37ad24b814de
K3r0m <109816170+K3r0m@users.noreply.github.com>
2025-03-18 10:33:59 +0100

    Update Wan_Install.bash

--- Wan_Install.bash
+++ Wan_Install.bash
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 # Erstelle die Verzeichnisse, falls sie nicht existieren
-mkdir -p SwarmUI/Models/diffusion_models
-mkdir -p SwarmUI/Models/clip
-mkdir -p SwarmUI/Models/vae
-mkdir -p SwarmUI/Models/clip_vision
+mkdir -p /workspace/ComfyUI/models/diffusion_models
+mkdir -p /workspace/ComfyUI/models/text_encoders
+mkdir -p /workspace/ComfyUI/models/vae
+mkdir -p /workspace/ComfyUI/models/clip_vision
 
 # Dateien nacheinander herunterladen
-curl -L -o SwarmUI/Models/diffusion_models/wan2.1_i2v_480p_14B_fp8_scaled.safetensors https://huggingface.co/Comfy-Org/Wan_2.1_ComfyUI_repackaged/resolve/main/split_files/diffusion_models/wan2.1_i2v_480p_14B_fp8_scaled.safetensors
+curl -L -o /workspace/ComfyUI/models/diffusion_models/wan2.1_i2v_720p_14B_fp8_scaled.safetensors https://huggingface.co/Comfy-Org/Wan_2.1_ComfyUI_repackaged/resolve/main/split_files/diffusion_models/wan2.1_i2v_720p_14B_fp8_scaled.safetensors
 
-curl -L -o SwarmUI/Models/clip/umt5_xxl_fp8_e4m3fn_scaled.safetensors "https://huggingface.co/Comfy-Org/Wan_2.1_ComfyUI_repackaged/resolve/main/split_files/text_encoders/umt5_xxl_fp8_e4m3fn_scaled.safetensors"
+curl -L -o /workspace/ComfyUI/models/text_encoders/umt5_xxl_fp8_e4m3fn_scaled.safetensors "https://huggingface.co/Comfy-Org/Wan_2.1_ComfyUI_repackaged/resolve/main/split_files/text_encoders/umt5_xxl_fp8_e4m3fn_scaled.safetensors"
 
-curl -L -o SwarmUI/Models/VAE/wan_2.1_vae.safetensors "https://huggingface.co/Comfy-Org/Wan_2.1_ComfyUI_repackaged/resolve/main/split_files/vae/wan_2.1_vae.safetensors"
+curl -L -o /workspace/ComfyUI/models/vae/wan_2.1_vae.safetensors "https://huggingface.co/Comfy-Org/Wan_2.1_ComfyUI_repackaged/resolve/main/split_files/vae/wan_2.1_vae.safetensors"
 
-curl -L -o SwarmUI/Models/clip_vision/clip_vision_h.safetensors "https://huggingface.co/Comfy-Org/Wan_2.1_ComfyUI_repackaged/resolve/main/split_files/clip_vision/clip_vision_h.safetensors"
+curl -L -o /workspace/ComfyUI/models/clip_vision/clip_vision_h.safetensors "https://huggingface.co/Comfy-Org/Wan_2.1_ComfyUI_repackaged/resolve/main/split_files/clip_vision/clip_vision_h.safetensors"
 
 echo "Alle Downloads abgeschlossen."
