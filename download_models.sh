f648c2e1284e125df3371f5675009d7ebb030a8f
K3r0m <109816170+K3r0m@users.noreply.github.com>
2025-03-18 09:33:23 +0100

    Update download_models.sh

--- download_models.sh
+++ download_models.sh
@@ -7,7 +7,7 @@ mkdir -p /workspace/SwarmUI/Models/VAE
 mkdir -p /workspace/SwarmUI/Models/clip_vision
 
 # Dateien nacheinander herunterladen
-curl -L -o SwarmUI/Models/diffusion_models/wan2.1_i2v_480p_14B_fp8_scaled.safetensors https://huggingface.co/Comfy-Org/Wan_2.1_ComfyUI_repackaged/resolve/main/split_files/diffusion_models/wan2.1_i2v_480p_14B_fp8_scaled.safetensors
+curl -L -o /workspace/SwarmUI/Models/diffusion_models/wan2.1_i2v_720p_14B_fp8_scaled.safetensors https://huggingface.co/Comfy-Org/Wan_2.1_ComfyUI_repackaged/resolve/main/split_files/diffusion_models/wan2.1_i2v_720p_14B_fp8_scaled.safetensors
 
 curl -L -o /workspace/SwarmUI/Models/clip/umt5_xxl_fp8_e4m3fn_scaled.safetensors "https://huggingface.co/Comfy-Org/Wan_2.1_ComfyUI_repackaged/resolve/main/split_files/text_encoders/umt5_xxl_fp8_e4m3fn_scaled.safetensors"
 
